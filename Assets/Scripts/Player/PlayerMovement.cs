df277db5d128d23a311342a1f9e08b2f7c42b9f3
Aravill <mozikmichal@gmail.com>
2025-12-11 16:54:36 +0100

    wip movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,10 +3,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+  [Header("Misc")]
+  private CharacterController _charController;
+
   [Header("Camera Settings")]
   [SerializeField] private Camera playerCamera;
   [SerializeField] private float mouseSensitivity = 100f;
   [SerializeField] private float maxLookUpAngle = 80f; // Prevents looking past overhead
+
+
+  [Header("Movement Settings")]
+  [SerializeField] private float acceleration = 0.2f;
+  [SerializeField] private float deceleration = 0.2f;
+  [SerializeField] private float maxMoveSpeed = 1f;
+
+  private PlayerInputActions _actions;
+  private InputAction _movementAction;
+  private float gravity = 9.81f;
 
   private float xRotation = 0f;
 
@@ -16,11 +29,64 @@ public class PlayerMovement : MonoBehaviour
     Cursor.lockState = CursorLockMode.Locked;
   }
 
+  void Awake()
+  {
+    _charController = GetComponent<CharacterController>();
+    _actions = new PlayerInputActions();
+    _movementAction = _actions.Movement.Move;
+  }
+
   void Update()
   {
     HandleMouseLook();
+    HandleMovement();
   }
 
+  private void OnEnable()
+  {
+    _actions.Movement.Enable();
+  }
+
+  private void OnDisable()
+  {
+    _actions.Movement.Disable();
+  }
+
+  private void HandleMovement()
+  {
+    Vector2 input = _movementAction.ReadValue<Vector2>();
+
+    Vector3 forward = playerCamera.transform.forward;
+    Vector3 right = playerCamera.transform.right;
+
+    forward.y = 0;
+    right.y = 0;
+
+    forward.Normalize();
+    right.Normalize();
+
+    Vector3 targetDirection = (forward * input.y + right * input.x).normalized;
+    Vector3 targetVelocity = targetDirection * maxMoveSpeed;
+
+    if (input.magnitude > 0)
+    {
+      // Accelerate towards target velocity
+      targetDirection = Vector3.MoveTowards(_charController.velocity, targetVelocity, acceleration * Time.deltaTime);
+    }
+    else
+    {
+      // Decelerate to a stop
+      targetDirection = Vector3.MoveTowards(_charController.velocity, Vector3.zero, deceleration * Time.deltaTime);
+    }
+
+    if (!_charController.isGrounded)
+    {
+      Vector3 gravityVector = Vector3.down * gravity * Time.deltaTime;
+      targetDirection += gravityVector;
+    }
+
+    _charController.Move(targetDirection);
+  }
   private void HandleMouseLook()
   {
     if (playerCamera == null)
